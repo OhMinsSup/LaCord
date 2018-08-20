6bfc1e56e799a5345d50d44f7efa8bc30b03ca7b
Your Name <you@example.com>
2018-08-21 00:49:59 +0900

    lacord/ google map api

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,26 +3,11 @@ import auth from './auth';
 
 const router = new Router();
 
-import { getElevation } from '../lib/GoogleMap';
-
 router.use('/auth', auth.routes());
 
 // test api
 router.get('/', (ctx) => {
     ctx.body = 'test';
 });
-
-router.get('/test', async (ctx) => {
-    const data = [{
-        lat: 40.71189,
-        lng: -111.96794
-      }, {
-        lat: 40.71189,
-        lng: -112.9679
-      }]
-    const a = await getElevation(data);
-
-    ctx.body = a;
-})
 
 export default router;
